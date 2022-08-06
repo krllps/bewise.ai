540f88fcdecd80bd8bc1669ffce6ce2024987768
krllps <renat.kyryllov@gmail.com>
2022-08-06 16:11:35 +0300

    Create QuizItem model
    Created quiz_item model (id, question, answer, created_at, saved_at)
    Created alembic migration

--- src/app/models.py
+++ src/app/models.py
@@ -1,1 +1,21 @@
+import sqlalchemy as sqla
+
 from .db import Base
+
+
+class QuizItem(Base):
+    __tablename__ = "quiz_item"
+    __mapper_args__ = {
+        "eager_defaults": True
+    }
+
+    id = sqla.Column(sqla.Integer, primary_key=True)
+    question = sqla.Column(sqla.String(512), nullable=False, index=True)
+    answer = sqla.Column(sqla.String(512), nullable=False)
+    created_at = sqla.Column(sqla.TIMESTAMP, nullable=False)
+
+    saved_at = sqla.Column(sqla.TIMESTAMP, nullable=True, server_default=sqla.func.now(), index=True)
+
+    def __repr__(self):
+        return f"QuizItem. id: {self.id}, question: {self.question}, answer: {self.answer}," \
+               f" created_at: {self.created_at}"

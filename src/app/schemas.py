01c40ce4da7ac525c84d53e927e7827014c3fd98
krllps <renat.kyryllov@gmail.com>
2022-08-08 15:30:22 +0300

    Create validators
    Created JServiceAPIResponse, CountParameter schemas

---
+++ src/app/schemas.py
@@ -0,0 +1,17 @@
+from pydantic import BaseModel, Field
+
+from datetime import datetime
+
+
+class JServiceAPIResponse(BaseModel):
+    id: int
+    question: str
+    answer: str
+    created_at: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class CountParameter(BaseModel):
+    count: int = Field(default=..., gt=0, le=100)
